795ba30751bdc175f53f03f05a26b4fdec7b21c0
James Lewandowski <jamesrlew@gmail.com>
2019-12-10 14:50:14 -0600

    ugh

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/jameslewandowski/Documents/WebDevelopment/fabler-presentation/src/layouts/index.js"
+  import data from "/Users/jameslewandowski/Documents/WebDevelopment/fabler-presentation/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Fabler',
+  },
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+}
