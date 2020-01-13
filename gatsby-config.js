18ae350471aa9a76831ad44488aeb86d6153a740
James Lewandowski <jamesrlew@gmail.com>
2020-01-13 17:47:38 -0600

    Minor Updates to Footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Fabler',
+    descrition: 'Capstone Project for James Lewandowski : A prototype of a Story Planning application.',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-sass`],
 }
